31de14f12c40d71c5dc2f788c6730516a0936ce4
Arian Sani <ariansani@hotmail.com>
2022-09-21 12:28:39 +0800

    added AmazonS3 client for spaces upload

--- src/main/java/vttpnusiss/day37upload/controllers/UploadRestController.java
+++ src/main/java/vttpnusiss/day37upload/controllers/UploadRestController.java
@@ -2,7 +2,11 @@ package vttpnusiss.day37upload.controllers;
 
 import java.io.IOException;
 import java.sql.ResultSet;
+import java.util.Date;
+import java.util.HashMap;
+import java.util.Map;
 import java.util.Optional;
+import java.util.UUID;
 
 import javax.websocket.server.PathParam;
 
@@ -20,6 +24,12 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestPart;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.multipart.MultipartFile;
+
+import com.amazonaws.services.s3.AmazonS3Client;
+import com.amazonaws.services.s3.model.CannedAccessControlList;
+import com.amazonaws.services.s3.model.ObjectMetadata;
+import com.amazonaws.services.s3.model.PutObjectRequest;
+import com.amazonaws.services.s3.model.PutObjectResult;
 
 import jakarta.json.Json;
 import jakarta.json.JsonObject;
@@ -40,6 +50,9 @@ public class UploadRestController {
     @Autowired
     private UploadService uploadSvc;
 
+    @Autowired
+    private AmazonS3Client s3Client;
+
     @GetMapping(path = "{id}")
     public ResponseEntity<byte[]> getUpload(@PathVariable Integer id) {
         Optional<Upload> opt = uploadSvc.getPost(id);
@@ -50,9 +63,47 @@ public class UploadRestController {
                 .body(up.getContent());
     }
 
+    @PostMapping(path = "/spaces", consumes = MediaType.MULTIPART_FORM_DATA_VALUE)
+    public ResponseEntity<String> postSpacesUpload(@RequestPart MultipartFile myfile, @RequestPart String title) {
+
+        // my private metadata
+        Map<String, String> myData = new HashMap<>();
+        myData.put("title", title);
+        myData.put("createdOn", (new Date()).toString());
+
+        // metadata for the object
+        ObjectMetadata metadata = new ObjectMetadata();
+        metadata.setContentType(myfile.getContentType());
+        metadata.setContentLength(myfile.getSize());
+        metadata.setUserMetadata(myData);
+
+        String hash = UUID.randomUUID().toString().substring(0, 8);
+
+        try {
+
+            PutObjectRequest putReq = new PutObjectRequest("arianBucket", "bear/%s".formatted(hash),
+                    myfile.getInputStream(), metadata);
+            putReq = putReq.withCannedAcl(CannedAccessControlList.PublicRead);
+            PutObjectResult result = s3Client.putObject(putReq);
+
+        } catch (IOException e) {
+
+            // TODO Auto-generated catch block
+            e.printStackTrace();
+        }
+
+        JsonObject data = Json.createObjectBuilder()
+                .add("content-type", myfile.getContentType())
+                .add("name", hash)
+                .add("original_name", myfile.getOriginalFilename())
+                .add("size", myfile.getSize())
+                .add("form_title", title)
+                .build();
+        return ResponseEntity.ok(data.toString());
+    }
+
     @PostMapping(consumes = MediaType.MULTIPART_FORM_DATA_VALUE)
-    public ResponseEntity<String> postUpload(
-            @RequestPart MultipartFile myfile, @RequestPart String title, @RequestPart String email) {
+    public ResponseEntity<String> postUpload(@RequestPart MultipartFile myfile, @RequestPart String title) {
 
         try {
             int updated = template.update(SQL_INSERT_BLOB, title, myfile.getInputStream(), myfile.getContentType());
@@ -71,7 +122,6 @@ public class UploadRestController {
                 .add("original_name", myfile.getOriginalFilename())
                 .add("size", myfile.getSize())
                 .add("form_title", title)
-                .add("form_email", email)
                 .build();
         return ResponseEntity.ok(data.toString());
     }
